689b8150db1db76e3a96089e1ecdf9573faa323b
Alex Paz <alexmpaz@yahoo.com>
2022-01-18 11:45:51 +0000

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Alex Paz on 18/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
