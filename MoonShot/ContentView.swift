a6b144eabd62bbfee7d496e8d499ee4361f01912
Alex Paz <alexmpaz@yahoo.com>
2022-01-18 19:01:30 +0000

    GeometryReader intro

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            Image("example")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geo.size.width * 0.8)
+                .frame(width: geo.size.width, height: geo.size.height)
+                //.clipped()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
